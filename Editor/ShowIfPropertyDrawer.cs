29822949705634bd5e44a90a8dcb4d96879e1065
EnjoyW0rld <denisavatar@hotmail.com>
2026-01-06 19:51:05 +0200

    Parameters upd

--- Editor/ShowIfPropertyDrawer.cs
+++ Editor/ShowIfPropertyDrawer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using System.Reflection;
+using System;
 
 namespace QOLAttributes
 {
@@ -11,6 +12,7 @@ namespace QOLAttributes
     public class ShowIfPropertyDrawer : PropertyDrawer
     {
         private FieldInfo propInfo;
+        private bool _isSeen;
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             if (propInfo == null)
@@ -22,29 +24,41 @@ namespace QOLAttributes
             {
                 return;
             }
-            base.OnGUI(position, property, label);
+            EditorGUILayout.PropertyField(property, label);
+            //base.OnGUI(position, property, label);
         }
-        public bool EvaluateShowIf(SerializedProperty pProperty)
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return 0;
+        }
+        private bool EvaluateShowIf(SerializedProperty pProperty)
         {
             ShowIfAttribute showIf = (ShowIfAttribute)attribute;
+            object propertyValue = propInfo.GetValue(pProperty.serializedObject.targetObject);
+            if (showIf.CompType == ShowIfAttribute.ComparisonType.IsNull || showIf.CompType == ShowIfAttribute.ComparisonType.IsNotNull)
+            {
+                return DoNullEvalutionCheck(propertyValue);
+            }
+            IComparable val1 = Convert.ChangeType(showIf.TargetValue, propertyValue.GetType()) as IComparable;
+            IComparable val2 = propertyValue as IComparable;
             switch (showIf.CompType)
             {
-                case ShowIfAttribute.ComparisonType.Les:
-                    break;
+                case ShowIfAttribute.ComparisonType.Less:
+                    return val1.CompareTo(val2) > 0;
                 case ShowIfAttribute.ComparisonType.Equal:
-                    if (propInfo.GetValue(pProperty.serializedObject.targetObject).Equals(showIf.TargetValue))
-                    {
-                        return true;
-                    }
-                    break;
+                    return val1.CompareTo(val2) == 0;
                 case ShowIfAttribute.ComparisonType.NotEqual:
-                    break;
+                    return val1.CompareTo(val2) != 0;
                 case ShowIfAttribute.ComparisonType.Bigger:
-                    break;
+                    return val1.CompareTo(val2) < 0;
             }
 
             return false;
         }
-
+        private bool DoNullEvalutionCheck(object pPropertyValue)
+        {
+            bool isNullCheck = ((ShowIfAttribute)attribute).CompType == ShowIfAttribute.ComparisonType.IsNull;
+            return (isNullCheck ? pPropertyValue.Equals(null) : !pPropertyValue.Equals(null));
+        }
     }
 }
--- Runtime/ShowIfAttribute.cs
+++ Runtime/ShowIfAttribute.cs
@@ -11,13 +11,30 @@ namespace QOLAttributes
     {
         public string ValueToComparer;
         public object TargetValue;
-        public enum ComparisonType { Les, Equal, NotEqual, Bigger }
+        public enum ComparisonType { Less, Equal, NotEqual, Bigger, IsNull, IsNotNull }
         public ComparisonType CompType;
-        public ShowIfAttribute(ComparisonType pComparer, string pValueToCompare,object pTargetValue)
+        public ShowIfAttribute(ComparisonType pComparer, string pValueToCompare, object pTargetValue)
         {
             ValueToComparer = pValueToCompare;
             CompType = pComparer;
             TargetValue = pTargetValue;
+        }
+        /// <summary>
+        /// Constructor used ONLY for null/not null comparion, make sure
+        /// nullable type is passed, otherwise it would not work
+        /// </summary>
+        /// <param name="pValueToCompare"></param>
+        /// <param name="pComparer">Can only be IsNull and IsNotNull</param>
+        public ShowIfAttribute(string pValueToCompare, ComparisonType pComparer = ComparisonType.IsNotNull)
+        {
+            ValueToComparer = pValueToCompare;
+            if (pComparer != ComparisonType.IsNull && pComparer != ComparisonType.IsNotNull)
+            {
+                Debug.LogError($"Using shot attribute constructor wrong! Passed compare value - {pComparer} is incorrect for this type of construcor!");
+                CompType = ComparisonType.IsNotNull;
+                return;
+            }
+            CompType = pComparer;
         }
     }
 
